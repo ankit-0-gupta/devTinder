1cfe441af7812b2aa6f4618b7fcd71454f66628e
Ankit Gupta <ankit05gupta01@gmail.com>
2024-10-10 23:10:55 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.use("/user", (req, res)=>{
-    res.send("User!!");
-});
-
-// This will only handle GET call to /user
-app.get("/user", (req, res)=>{
+app.get("/user/:userId/:age", (req, res)=>{
+    console.log(req.params.userId);
     res.send({userName: 'Ankit Gupta', age: 21});
-});
-
-app.post("/user", (req, res)=>{
-    // saving data to DB
-    res.send("Data successfully saved to the database");
-});
-
-app.delete("/user", (req, res)=>{
-    res.send("Deleted Successfully!");
-});
-
-
-// This will match all the HTTP method API calls to /test
-app.use("/test", (req, res)=>{
-    res.send("Testing!!");
 });
 
 app.listen(3000, () => {
