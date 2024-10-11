c8d08d2eb9b99499bcf6eb6f19eb6ce83680a2ef
Ankit Gupta <ankit05gupta01@gmail.com>
2024-10-11 23:22:17 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.get("/user/:userId/:userName/:userAge", (req, res)=>{
-    console.log(req.params.userId);
-    res.send({userName: 'Ankit Gupta', age: 21});
+// app.use("/route", [rH, rH2], rH3, rH4);
+
+app.get("/user", (req, res, next) => {
+    console.log("Handling the route user!!");
+    // res.send("Response!!");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 2!!");
+    // res.send("2nd Response!!");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 3!!");
+    // res.send("3rd Response!!");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 4!!");
+    // res.send("4th Response!!");
+    next();
+},
+(req, res, next) => {
+    console.log("Handling the route user 5!!");
+    res.send("5th Response!!");
 });
 
 app.listen(3000, () => {
